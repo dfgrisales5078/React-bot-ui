02294a3cb918be6ebdd4d5ca01cbe45901d6f705
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2022-09-19 14:38:46 -0400

    added space between input & button divs

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 const HomePage = () => {
     return (
         <React.Fragment>
-        <h1>Enter your Instagram username:</h1>
+            <h1>Enter your Instagram:</h1>
             <form>
-              <div class="form-floating">
+              <div className="form-floating">
                 <input type="email" class="form-control" id="floatingInput" placeholder="@example" input/>
                 <label for="floatingInput">username</label>
               </div>
-    
-              <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+              <div class="mt-3 col-md-12">
+                <button class="w-100 btn btn-lg btn-primary mr-1" type="submit">Get likes!</button>
+              </div>
             </form>
         </React.Fragment>
     );
