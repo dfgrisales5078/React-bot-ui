c6cc112b128c2137da13267a19c2f8e3d702c469
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2022-09-20 09:56:29 -0400

    added placeholder to username input box

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -5,15 +5,15 @@ const HomePage = () => {
         <React.Fragment>
             <h1>Enter your Instagram:</h1>
             <form>
-              <div className="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="@example" input/>
-                <label for="floatingInput">username</label>
-              </div>
+            <div class="form-group">
+              <label for="usernameInput">Username:</label>
+              <input type="text" class="form-control" id="usernameInput" placeholder="@IG_username" input/>
+            </div>
               <div class="mt-3 col-md-12">
                 <button class="w-100 btn btn-lg btn-primary mr-1" type="submit">Get likes!</button>
               </div>
             </form>
-        </React.Fragment>
+        </React.Fragment> 
     );
 }
 
